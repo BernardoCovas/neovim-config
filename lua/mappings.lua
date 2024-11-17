68acf19a64bf6cadbdfeb4041382bd97a16471f4
Bernardo Covas <bernardo.covas@outlook.com>
2024-11-17 20:58:55 +0000

    tab next and previous

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader><tab><tab>", ":tabn<enter>", { desc = "tab next" })
+map("n", "<leader><tab><S-tab>", ":tabp<enter>", { desc = "tab prev" })
 
 map({ "n", "t" }, "<A-i>", function()
   require("nvchad.term").toggle {
