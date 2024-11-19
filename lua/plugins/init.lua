febe8b240e982cf8b60c847f640d8ecb0d8bec87
Bernardo Covas <bernardo.covas@outlook.com>
2024-11-19 17:12:19 +0000

    Cleaned plugins, auto-format enable / disable

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,14 +1,21 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = "BufWritePre", -- format on save
-    opts = require "configs.conform",
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
+    end
   },
-
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require "configs.gitsigns"
     end,
   },
 }
