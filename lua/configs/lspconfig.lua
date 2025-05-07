f5459fa4a3684dbbf1b20c45d4ebc8faaa9731bc
Bernardo Covas <bernardo.covas@outlook.com>
2025-05-07 16:37:38 +0000

    update to new neovim 0.11, lspconfig changed

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,20 +1,7 @@
 require("mason").setup()
 
 local nvlsp = require "nvchad.configs.lspconfig"
-local lspconfig = require "lspconfig"
 local mason_lspconfig = require "mason-lspconfig"
 
 mason_lspconfig.setup()
 nvlsp.defaults() -- loads nvchad's defaults
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    lspconfig[server_name].setup {
-      on_attach = nvlsp.on_attach,
-      on_init = nvlsp.on_init,
-      capabilities = nvlsp.capabilities,
-    }
-  end,
-}
-
--- require("nvchad.configs.lspconfig").defaults()
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,5 +20,4 @@ return {
       require "configs.lspconfig"
     end
   },
-  "neovim/nvim-lspconfig",
 }
